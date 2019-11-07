7bbb6b1e7badff8e6754588477377597e31376f6
kernalbin <sphunts01@gmail.com>
2019-11-07 10:00:24 -0700

    Began Project

--- app/src/main/java/com/example/jatctimeclock/MainActivity.java
+++ app/src/main/java/com/example/jatctimeclock/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.jatctimeclock;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Connect button
+        // ..get the button
+        Button btn_in = (Button) findViewById(R.id.button_clock_in);
+        // ..set what happens when the user clicks
+        btn_in.setOnClickListener();
     }
 }
